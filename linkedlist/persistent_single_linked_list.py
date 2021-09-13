3b9f82cd486ad4661b53fffc24eb650508f3dec1
cikay <muzaffer123cikay@gmail.com>
2021-09-13 22:20:12 +0300

    Add delete commit features

--- linkedlist/persistent_single_linked_list.py
+++ linkedlist/persistent_single_linked_list.py
@@ -1,9 +1,12 @@
+import copy
+
 
 class Commit:
 
     def __init__(self, message):
-        self.message = message
         self.next = None
+        self.message = message
+        self.related_branchs = []
 
 class Branch:
 
@@ -17,8 +20,8 @@ class Git:
     def __init__(self):
         self.branchs = []
         master_branch = Branch('master', None)
-        self.branchs.append(master_branch)
         self.current_branch = master_branch
+        self.branchs.append(master_branch)
 
     def add_commit(self, message):
         commit = Commit(message)
@@ -37,6 +40,7 @@ class Git:
         branch = Branch(branch_name, head_commit)
         self.current_branch = branch
         self.branchs.append(branch)
+        head_commit.related_branchs.append(branch)
         print(f'Switched to a new branch: {branch_name}')
 
     def change_branch(self, branch_name):
@@ -45,6 +49,22 @@ class Git:
                 self.current_branch = branch
                 print(f'Switched to branch: {self.current_branch.name}')
                 break
+
+    def delete_commit(self, commit_message):
+        head_commit = self.current_branch.head_commit
+        if head_commit.message == commit_message:
+            self.current_branch.head_commit = head_commit.next
+            return
+
+        iter_ = copy.deepcopy(head_commit)
+        self.current_branch.head_commit = iter_
+        while iter_.next is not None:
+            if iter_.next.message == commit_message:
+                iter_.next = iter_.next.next
+                break
+
+            iter_.next = copy.copy(iter_.next)
+            iter_ = iter_.next
 
     def log_graph(self):
         pass
@@ -60,16 +80,20 @@ class Git:
         print(f'Current branch: {self.current_branch.name}')
 
 git = Git()
-commit_messages = ["first commit", "Implement Instagram"]
+commit_messages = ["first commit", "Implement Instagram", 'Implement share event']
 for commit_message in commit_messages:
     git.add_commit(commit_message)
 
 git.create_new_branch('new_feature')
 git.log_current_branch()
 git.add_commit('Implement receive post event')
+git.add_commit('Implement story off event')
 git.change_branch('master')
 git.log_current_branch()
 git.add_commit('Handle error')
+git.change_branch('new_feature')
+git.delete_commit('Implement Instagram')
+current = git.current_branch.head_commit
 for branch in git.get_branchs():
     git.change_branch(branch.name)
     git.log()
