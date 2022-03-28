8f2a6a33017adeccd6435c0476d1054be2fa7e94
cikay <muzaffer123cikay@gmail.com>
2022-03-28 13:42:12 +0300

    Add trie data structures

---
+++ Trie/trie.py
@@ -0,0 +1,44 @@
+
+
+class Trie:
+
+    def __init__(self) -> None:
+        self.nodes = {}
+        self.is_leaf = False
+    
+
+    def insert(self, word):
+        current_trie = self
+        for char in word:
+            if char not in current_trie.nodes:
+                current_trie.nodes[char] = Trie()
+            
+            current_trie = current_trie.nodes[char]
+
+        current_trie.is_leaf = True
+
+
+    def search(self, word):
+        current_trie = self
+        for char in word:
+            if char not in current_trie.nodes:
+                return False
+            
+            current_trie = current_trie.nodes[char]
+
+        return current_trie.is_leaf
+
+
+trie = Trie()
+trie.insert('Muzaffer')
+is_muzaffer_found = trie.search('Muzaffer')
+is_imam_found = trie.search('İmam')
+print(f'is_muzaffer_found: {is_muzaffer_found}')
+print(f'is_imam_found: {is_imam_found}')
+print('After imam inserted')
+trie.insert('İmam')
+is_imam_found = trie.search('İmam')
+print(f'is_imam_found: {is_imam_found}')
+
+            
+
