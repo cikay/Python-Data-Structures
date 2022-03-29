7838ca14f86cb140288db3329d0083d3f506e3fa
cikay <muzaffer123cikay@gmail.com>
2022-03-29 22:23:28 +0300

    Create hash table by list comprension
    To create different instance of linked lists

--- hashTable/hashtable.py
+++ hashTable/hashtable.py
@@ -20,7 +20,7 @@ class Node:
 class HashTable:
 
     def __init__(self, size) -> None:
-        self.table = [LinkedListNode()]*size
+        self.table = [LinkedListNode() for _ in range(size)]
 
 
     def insert(self, value):
