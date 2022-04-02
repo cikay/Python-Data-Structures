f2b476c1dd1308515f0e9cae4bf4fcdd384ebad2
cikay <muzaffer123cikay@gmail.com>
2022-04-02 20:51:56 +0300

    Use is when check None

--- linkedlist/singly_linked_list.py
+++ linkedlist/singly_linked_list.py
@@ -37,7 +37,7 @@ class LinkedList:
 
         iter = self.head
 
-        if self.head == None: #
+        if self.head is None:
             return Node(data)
         
         elif data < self.head.data: #root change
